a7f02ba4d84530883ef168f0b46c55e078ccaa9e
unknown <sahajkhalasi69@gmail.com>
2024-03-07 18:56:05 +0530

    props

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./Header"
+import Slider from "./Slider"
+import Footer from "./Footer"
+import Product from "./Product"
+
+function App() {
+    return (
+        <div>
+        <Header />
+        <Slider/>
+        <Product/>
+        <Footer/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Footer.js
@@ -0,0 +1,5 @@
+function Footer() {
+    return <div style={{backgroundColor: "purple", height: "40vh"}}><h1>Footer</h1></div>
+}
+
+export default Footer
